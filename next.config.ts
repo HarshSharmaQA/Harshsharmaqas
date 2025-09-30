5685fd435be80de4c17149f54346aa0d122ba495
Harsh Sharma QA <harshsharmaqa@gmail.com>
2025-09-30 13:57:16 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,13 +29,18 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       }
     ],
   },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+    ]
+  }
 };
 
 export default nextConfig;

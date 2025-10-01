c516a5a605da6f9c9edee41de4a88048231ac3e7
Harsh Sharma QA <harshsharmaqa@gmail.com>
2025-10-01 10:33:43 +0000

    Updated site settings and improved page layouts

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -46,6 +46,9 @@
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = __webpack_module_cache__;
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -125,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9c45d078e25b38fd")
+/******/ 		__webpack_require__.h = () => ("c1c6fffddea03580")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -215,6 +218,7 @@
 /******/ 	
 /************************************************************************/
 /******/ 	
+/******/ 	// module cache are used so entry inlining is disabled
 /******/ 	
 /******/ })()
 ;
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89ae6c23e24ad88a"; }
+/******/ 		__webpack_require__.h = function() { return "fbd9a7b4e96b23a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
